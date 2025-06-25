511a775320022d114781356e97085d1c83af679a
arashad202 <ahm.rashad.202@gmail.com>
2025-06-25 05:29:01 +0300

    Homepage
    Create homepage content in homepage.js import
    it in index.js and append it to container
    and configure clicks on nav buttons.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./styles.css";
+import { HomepageContent } from "./modules/homepage.js";
 
-console.log("Hello js");
+const content = document.querySelector('#content');
+const homeBtn = document.querySelector("#homepage");
+const menuBtn = document.querySelector("#menu");
+const aboutBtn = document.querySelector('#about');
+
+// default to homepage content
+const homepage = HomepageContent();
+content.appendChild(homepage);
+
+
+homeBtn.addEventListener('click', () => {
+    content.removeChild(content.firstChild);
+    const homepage = HomepageContent();
+    content.appendChild(homepage);
+});
+
+menuBtn.addEventListener('click', () => {
+    content.removeChild(content.firstChild);
+    content.appendChild(document.createElement('div'));
+});
+
+aboutBtn.addEventListener('click', () => {
+    content.removeChild(content.firstChild);
+    content.appendChild(document.createElement('div'));
+})
+
+
+
---
+++ src/modules/homepage.js
@@ -0,0 +1,29 @@
+import restaurantImage from "../assets/restaurant-pic.jpg"
+
+export function HomepageContent() {
+    // Container div
+    const container = document.createElement('div');
+    container.classList.add('w-[50%]', 'mx-auto');
+    // image
+    const image = document.createElement('img');
+    image.src = restaurantImage;
+    image.classList.add('mx-auto');
+    container.appendChild(image);
+    // heading
+    const header = document.createElement('h2');
+    header.classList.add('font-bold', 'text-3xl', 'text-center', 'my-3');
+    header.textContent = 'Wodingo';
+    container.appendChild(header);
+    // subheading
+    const subHeader = document.createElement('h3');
+    subHeader.classList.add('font-bold', 'text-2xl', 'my-2');
+    subHeader.textContent = 'Overview';
+    container.appendChild(subHeader);
+    // paragraph
+    const paragraph = document.createElement('p');
+    paragraph.textContent='Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate beatae officiis fugit, consequatur, repellendus facilis sed, illo voluptas nam autem praesentium ducimus ad quia? Corrupti aperiam dolores laboriosam sint. Libero!';
+    container.appendChild(paragraph);
+
+    return container;
+}
+
